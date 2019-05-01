341b8a1c96c8a4c086645a2245ba57487351debe
Askar <asikaer.alamujiang@accenture.com>
2019-05-02 02:38:30 +0800

    init

---
+++ src/main/java/com/taklip/jediorder/JediOrderApplication.java
@@ -0,0 +1,13 @@
+package com.taklip.jediorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediOrderApplication.class, args);
+	}
+
+}
