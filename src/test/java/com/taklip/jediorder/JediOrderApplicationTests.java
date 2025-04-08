cf7930b3bc90166e44bb09ba2122ecafe4707b31
Askar <>
2025-04-08 21:43:21 +0800

    mvn to gradle

--- src/test/java/com/taklip/jediorder/JediOrderApplicationTests.java
+++ src/test/java/com/taklip/jediorder/JediOrderApplicationTests.java
@@ -1,97 +1,97 @@
-package com.taklip.jediorder;
+// package com.taklip.jediorder;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+// import java.util.concurrent.BrokenBarrierException;
+// import java.util.concurrent.CyclicBarrier;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-import com.taklip.jediorder.bean.Id;
-import com.taklip.jediorder.service.IdService;
+// import com.taklip.jediorder.bean.Id;
+// import com.taklip.jediorder.service.IdService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class JediOrderApplicationTests {
-	@Autowired
-	private IdService idService;
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class JediOrderApplicationTests {
+// 	@Autowired
+// 	private IdService idService;
 
-	@Test
-	public void contextLoads() {
-		final int number = 100;
+// 	@Test
+// 	public void contextLoads() {
+// 		final int number = 100;
 
-		Thread[] treads = new Thread[number];
+// 		Thread[] treads = new Thread[number];
 
-		boolean flag = false;
+// 		boolean flag = false;
 
-		CyclicBarrier barrier = new CyclicBarrier(number, new BarrierRun(flag, number));
+// 		CyclicBarrier barrier = new CyclicBarrier(number, new BarrierRun(flag, number));
 
-		System.out.println("Start generate id.");
+// 		System.out.println("Start generate id.");
 
-		for (int i = 0; i < number; ++i) {
-			System.out.println("Generator ID[" + i + "] started.");
+// 		for (int i = 0; i < number; ++i) {
+// 			System.out.println("Generator ID[" + i + "] started.");
 
-			treads[i] = new Thread(new IDGenerator(i, barrier));
+// 			treads[i] = new Thread(new IDGenerator(i, barrier));
 
-			treads[i].start();
-		}
-	}
+// 			treads[i].start();
+// 		}
+// 	}
 
-	public class IDGenerator implements Runnable {
-		private Integer genId;
-		private final CyclicBarrier barrier;
+// 	public class IDGenerator implements Runnable {
+// 		private Integer genId;
+// 		private final CyclicBarrier barrier;
 
-		IDGenerator(Integer genId, CyclicBarrier barrier) {
-			this.genId = genId;
-			this.barrier = barrier;
-		}
+// 		IDGenerator(Integer genId, CyclicBarrier barrier) {
+// 			this.genId = genId;
+// 			this.barrier = barrier;
+// 		}
 
-		@Override
-		public void run() {
-			try {
-				barrier.await();
+// 		@Override
+// 		public void run() {
+// 			try {
+// 				barrier.await();
 
-				doGenerator();
+// 				doGenerator();
 
-				barrier.await();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-		}
+// 				barrier.await();
+// 			} catch (InterruptedException e) {
+// 				e.printStackTrace();
+// 			} catch (BrokenBarrierException e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
 
-		void doGenerator() {
-			long id = idService.generateId();
+// 		void doGenerator() {
+// 			long id = idService.generateId();
 
-			System.out.println("Generator ID[" + genId + "] create UNIQUE ID:" + id);
+// 			System.out.println("Generator ID[" + genId + "] create UNIQUE ID:" + id);
 
-			Id idObj = idService.explainId(id);
+// 			Id idObj = idService.explainId(id);
 
-			System.out.println("Generator ID[" + genId + "] explain UNIQUE ID[" + id + "], result:" + idObj);
-		}
-	}
+// 			System.out.println("Generator ID[" + genId + "] explain UNIQUE ID[" + id + "], result:" + idObj);
+// 		}
+// 	}
 
-	public class BarrierRun implements Runnable {
-		boolean flag;
-		int number;
+// 	public class BarrierRun implements Runnable {
+// 		boolean flag;
+// 		int number;
 
-		BarrierRun(boolean flag, int number) {
-			this.flag = flag;
-			this.number = number;
-		}
+// 		BarrierRun(boolean flag, int number) {
+// 			this.flag = flag;
+// 			this.number = number;
+// 		}
 
-		@Override
-		public void run() {
-			if (flag) {
-				System.out.println("All Generator [" + number + "] completed.");
-			}
-			else {
-				System.out.println("All Generator [" + number + "] initialized.");
-				flag = true;
-			}
-		}
-	}
-}
+// 		@Override
+// 		public void run() {
+// 			if (flag) {
+// 				System.out.println("All Generator [" + number + "] completed.");
+// 			}
+// 			else {
+// 				System.out.println("All Generator [" + number + "] initialized.");
+// 				flag = true;
+// 			}
+// 		}
+// 	}
+// }
